c32ff7cb427971abcb9c93f0e8cc4d104c089863
Alexey <alexryz2006@mail.ru>
2026-04-25 23:31:21 +0300

    refactor: update HotelResponse to match specification (address as string)

--- src/main/java/org/testwork/gp_hotels/dto/response/HotelResponse.java
+++ src/main/java/org/testwork/gp_hotels/dto/response/HotelResponse.java
@@ -1,5 +1,7 @@
 package org.testwork.gp_hotels.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.testwork.gp_hotels.entity.Address;
 @Data
@@ -7,6 +9,18 @@ public class HotelResponse {
     private Long id;
     private String name;
     private String description;
+    @JsonIgnore
     private Address address;
+
+    @JsonProperty("address")
+    public String getAddressAsString() {
+        return String.format("%s %s, %s, %s, %s",
+                address.getHouseNumber(),
+                address.getStreet(),
+                address.getCity(),
+                address.getPostCode(),
+                address.getCountry()
+        );
+    }
     private String phone;
 }
